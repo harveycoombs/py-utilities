920f36ca885a2173534b37060f7c1262a87bbb7b
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-03-09 22:23:24 +0000

    Model class methods

--- model.py
+++ model.py
@@ -3,8 +3,16 @@ import torch
 import torch.nn as nn
 
 class Model(nn.Module):
-  def __init__():
-    # to-do
+    def __init__(self, vocab_size, d_model=256, nhead=8, num_encoder_layers=6):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.transformer = nn.Transformer(d_model=d_model, nhead=nhead, num_encoder_layers=num_encoder_layers)
+        self.fc = nn.Linear(d_model, vocab_size)
+
+    def forward(self, src, tgt):
+        src, tgt = self.embedding(src), self.embedding(tgt)
+        output = self.transformer(src.permute(1, 0, 2), tgt.permute(1, 0, 2))
+        return self.fc(output.permute(1, 0, 2))
 
 class MaskedDataset(Dataset):
   # to-do
