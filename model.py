adefceb821e176f16250e07d6a251130260dcc42
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-03-09 22:20:55 +0000

    Initial structure

--- model.py
+++ model.py
@@ -1,3 +1,12 @@
 # Written by Harvey Coombs, 2023
 import torch
 import torch.nn as nn
+
+class Model(nn.Module):
+  def __init__():
+    # to-do
+
+class MaskedDataset(Dataset):
+  # to-do
+
+torch.save(model.state_dict(), "model.pth")
